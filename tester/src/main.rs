80c9db151eb12562f96576887da1e907c30e9947
Jacob Malloy <jmalloy1@vols.utk.edu>
2024-04-25 16:27:11 -0400

    modify the testing code

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -3,16 +3,22 @@ use std::time::Instant;
 
 
 fn main(){
+     
     let time1 = Instant::now();
     let mut tmp = BigInt::from(1);
     for i in 2..100001{
         tmp = &tmp * i;
     }
     let time_passed = time1.elapsed();
-/* 
-    for i in tmp.data.iter().rev(){
+    
+    let mut tmp2 = BigInt::from(1);
+    for i in 2..101{
+        tmp2 = &tmp2 * i;
+    }
+
+    for i in tmp2.data.iter().rev(){
         print!("{:016x}",i);
-    }*/
+    }
     println!();
     println!("{}",tmp.data.len());
     println!("{}s",time_passed.as_micros() as f64/1000000.0);
